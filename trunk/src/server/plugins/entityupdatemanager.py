2ee5f22a784e9671abcf8547108297ae3dc7b93c
Samuel Breese <sbreese@xitol.net>
2012-05-23 21:23:37 -0400

    Working, yay

--- trunk/src/server/plugins/entityupdatemanager.py
+++
@@ -1,9 +0,0 @@
-from ... import base
-from ... import utils
-from ... import level
-from ... import errors
-from ... import chameleon
-class entityupdatemanager(chameleon.listener):
-	def __init__(self, manager, controlledEntity):
-		chameleon.listener.__init__(self)
-		self.manager = manager
